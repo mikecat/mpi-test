91ff54cc1046bd4e6df870660ebd70092cce8ab9
MikeCAT <mikecat@users.noreply.github.com>
2018-05-18 16:35:10 +0900

    update vector.c

--- vector.c
+++ vector.c
@@ -16,19 +16,22 @@ int main(int argc, char* argv[]) {
 	int send[4] = {1, 2, 3, 4};
 	int recv[4] = {0, 0, 0, 0};
 	int rank;
-	MPI_Datatype dt;
+	MPI_Datatype dt, dt2;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	MPI_Type_vector(1, 1, 2, MPI_INT, &dt);
 	MPI_Type_commit(&dt);
+	MPI_Type_vector(2, 1, 2, MPI_INT, &dt2);
+	MPI_Type_commit(&dt2);
 
 	print_array(rank, "send", send, 4);
 	print_array(rank, "recv", recv, 4);
+	MPI_Barrier(MPI_COMM_WORLD);
 
 	if (rank == 0) {
 		MPI_Send(send, 2, dt, 1, 0, MPI_COMM_WORLD);
-	} else {
+	} else if (rank == 1) {
 		MPI_Status status;
 		int count;
 		MPI_Recv(recv, 2, dt, 0, 0, MPI_COMM_WORLD, &status);
@@ -38,6 +41,23 @@ int main(int argc, char* argv[]) {
 
 	print_array(rank, "send", send, 4);
 	print_array(rank, "recv", recv, 4);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	recv[0] = recv[1] = recv[2] = recv[3] = 0;
+
+	if (rank == 0) {
+		MPI_Send(send, 1, dt2, 1, 0, MPI_COMM_WORLD);
+	} else if (rank == 1) {
+		MPI_Status status;
+		int count;
+		MPI_Recv(recv, 1, dt2, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Get_count(&status, dt, &count);
+		printf("rank %d: received %d elements\n", rank, count);
+	}
+
+	print_array(rank, "send", send, 4);
+	print_array(rank, "recv", recv, 4);
+	MPI_Barrier(MPI_COMM_WORLD);
 
 	MPI_Finalize();
 }
